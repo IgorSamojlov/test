03d29f8136a0014bcdd2ffdda277d41127725fcc
Igor <igorsamojlov88@gmail.com>
2018-12-20 00:54:46 +0300

    temp

--- client.py
+++ client.py
@@ -3,18 +3,56 @@ import websockets
 import json
 import time
 
+
+async def reg(ws):
+    msg = {'cmd': 'reg', 'autn': str(time.time()),
+        'name': 'Igor', 'pasw':'1111','nick':'Igor_sam'}
+    mes = json.dumps(msg)
+
+    await ws.send(mes)
+
+async def get_user(ws):
+    msg = {'cmd': 'get_user', 'autn': str(time.time()),
+        'name': 'Igor', 'pasw':'1111','nick':'Igor_sam'}
+    mes = json.dumps(msg)
+    await ws.send(mes)
+    mes = await ws.recv()
+    print (mes)
+
+async def send_msg(ws, mg, adr):
+    msg = {'cmd':'send_msg', 'who':'Igor', 'mg':mg, 'adr':adr}
+    ms = json.dumps(msg)
+    await ws.send(ms)
+
 async def test():
 
     async with websockets.connect('ws://127.0.0.1:8765') as websocket:
 
-        msg = {'autn': str(time.time()),
-        'name': 'Igor', 'pasw':'1111','nick':'Igor_sam'}
-        mes = json.dumps(msg)
 
-        await websocket.send(mes)
+        while True:
+            cmd = input ('__ ')
+            if cmd == 'reg':
+                await reg(websocket)
 
-        response = await websocket.recv()
-        print(response)
+            elif cmd == 'get_user':
+                await get_user(websocket)
+
+            elif cmd == 'send_msg':
+                ad = input('Adress__ ')
+                ms = input ('Messge__ ')
+                await send_msg(websocket, ms, ad)
+
+            elif cmd == 'get':
+                a = await websocket.recv()
+                print (a)
+            elif cmd == 'quit':
+
+                break
+
+        #response = await websocket.recv()
+        #print(response)
+
+
         websocket.close()
 
 asyncio.get_event_loop().run_until_complete(test())
--- server.py
+++ server.py
@@ -18,17 +18,42 @@ async def send_all(ws):
     auth = list(US)
     for au in auth:
         if (US[au]['ws'].open):
-            await US[au]['ws'].send('You are here?\n')
+            print ('open')
+            await US[au]['ws'].send('Hello evere one\n')
+
+async def get_user(ws):
+    print ('Send user  ')
+    us = list(US)
+    print(us)
+    await ws.send(us)
+
+
+async def send_msg(ws, msg):
+    if (US[msg['adr']]['ws'].open):
+        m = (str(msg['mg']) + ' message from ' + str(msg['who']))
+        if (US[msg['adr']]['ws'].open):
+
+            await (US[msg['adr']]['ws'].send(m))
+
+        elif (not US[msg['adr']]['ws'].open):
+            print('offline\n')
 
 async def echo(websocket, path):
     async for message in websocket:
 
         print ('Join \n')
         print(websocket.remote_address, '\n')
-
         msg = (json.loads(message))
-        regis(websocket, msg)
-        await send_all(websocket)
+
+        if (msg['cmd'] == 'reg'):
+            regis(websocket, msg)
+        elif (msg['cmd'] == 'get_user'):
+            await get_user(websocket)
+
+        elif (msg['cmd'] == 'send_msg'):
+            await send_msg(websocket, msg)
+
+        #await send_all(websocket)
 
 asyncio.get_event_loop().run_until_complete(
     websockets.serve(echo, 'localhost', 8765))
